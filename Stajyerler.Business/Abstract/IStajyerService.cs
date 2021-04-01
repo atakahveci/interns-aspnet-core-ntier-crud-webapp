ff97d7e48b20ccb45cb2956d9548b1c10511f475
Atasagun <atasagunkahveci@gmail.com>
2021-04-01 11:50:09 +0300

    Initial Commited

---
+++ Stajyerler.Business/Abstract/IStajyerService.cs
@@ -0,0 +1,16 @@
+﻿using Stajyerler.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stajyerler.Business.Abstract
+{
+    public interface IStajyerService
+    {
+        List<Stajyer> GetAllStajyers();
+        Stajyer GetStajyerId(int id);
+        Stajyer CreateStajyer(Stajyer stajyer);
+        Stajyer UpdateStajyer(Stajyer stajyer);
+        void DeleteStajyer(int id);
+    }
+}
---
+++ Stajyerler.DataAccess/Migrations/20210219114321_initialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stajyerler.DataAccess.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stajyers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: true),
+                    Surname = table.Column<string>(maxLength: 50, nullable: true),
+                    University = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stajyers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stajyers");
+        }
+    }
+}
